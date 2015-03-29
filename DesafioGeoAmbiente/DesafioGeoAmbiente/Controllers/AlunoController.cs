45bb35c4c2643f9db9a0336ff0b4d53c613321ef
lucasribeiro <lucas.luisribeiro@gmail.com>
2015-03-29 14:38:17 -0300

    Desafio

---
+++ DesafioGeoAmbiente/DesafioGeoAmbiente/Controllers/AlunoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DesafioGeoAmbiente.Models;
+
+namespace DesafioGeoAmbiente.Controllers
+{
+    public class AlunoController : Controller
+    {
+        // GET: Aluno
+        public ActionResult Index()
+        {           
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Pesquisar()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Pesquisar(string ra)
+        {
+            IList<Aluno> Alunos = AbrirCsv();
+
+            Aluno aluno = Alunos.FirstOrDefault(a => a.RA == int.Parse(ra));
+
+            if (aluno == null) return View(); 
+               
+            return View(aluno);
+        }
+
+        private IList<Aluno> AbrirCsv()
+        {
+            IList<Aluno> Alunos = new List<Aluno>();
+
+            var appdata = HttpContext.Server.MapPath("~/App_Data/Alunos.csv"); 
+
+            StreamReader rd = new StreamReader(appdata);
+            string linha = null;
+            //string[] linhaseparada = null;
+            while ((linha = rd.ReadLine()) != null)
+            {
+                var colunas = linha.Split(';');
+                Aluno aluno = new Aluno();
+                aluno.Nome = colunas[0];
+                aluno.RA = int.Parse(colunas[1]);
+                aluno.lat = double.Parse(colunas[2]);
+                aluno.lon = double.Parse(colunas[3]);
+
+                Alunos.Add(aluno);
+
+            }
+            rd.Close();
+
+            return Alunos;
+        }
+    }
+}
---
+++ DesafioGeoAmbiente/DesafioGeoAmbiente/Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesafioGeoAmbiente.Models
+{
+    public class Aluno
+    {
+        public string  Nome { get; set; }
+        public int RA { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+}
